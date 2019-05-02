306e2a463790cf592bbc5b3a436d333afa3551b7
edurso <emdurso511@pccsk12.com>
2019-05-02 10:16:24 -0400

    framework started

---
+++ src/chess/Game.java
@@ -0,0 +1,12 @@
+package chess;
+import java.util.ArrayList;
+public class Game {
+	ArrayList<GamePiece> whitePieces;
+	ArrayList<GamePiece> blackPieces;
+	public Game() {
+		
+	}
+	public void run() {
+		
+	}
+}
