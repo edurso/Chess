335602d44a63b9c5dd09a6beb534f92564d06c22
edurso <eric@casadurso.com>
2019-06-04 09:32:34 -0400

    work

--- src/start/OnlineMenu.java
+++ src/start/OnlineMenu.java
@@ -3,8 +3,12 @@ package start;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
+
+import chess.Game;
 
 public class OnlineMenu extends JFrame{
 
@@ -19,6 +23,8 @@ public class OnlineMenu extends JFrame{
     JButton joinIt;
 
     JButton createNew;
+
+    JButton cancel;
 
     JTextField joinAddress;
 
@@ -35,19 +41,40 @@ public class OnlineMenu extends JFrame{
         create.setBackground(Color.BLACK);
         createNew = new JButton("Host Game");
         createNew.setBackground(Color.RED);
+        createNew.addActionListener(new ActionListener() { 
+            public void actionPerformed(ActionEvent e) { 
+                /*
+                Stuff to start a new game somehow
+                */ 
+            } 
+        });
+        cancel = new JButton("Cancel");
+        cancel.setBackground(Color.RED);
+        cancel.addActionListener(new ActionListener() { 
+            public void actionPerformed(ActionEvent e) { 
+                Game.menu.start();
+                dispose(); 
+            } 
+        });
         joinIt = new JButton("Join Server");
         joinIt.setBackground(Color.RED);
         joinAddress = new JTextField("Enter Address");
         joinAddress.setBackground(Color.BLACK);
         joinAddress.setForeground(Color.RED);
+        joinIt.addActionListener(new ActionListener() { 
+            public void actionPerformed(ActionEvent e) { 
+                /*
+                Stuff to take whatever is in joinAddress and pass it in to something to start game
+                */ 
+            } 
+        });
         join.add(joinAddress);
         join.add(joinIt);
         create.add(createNew);
+        create.add(cancel);
         main.add(join);
         main.add(create);
         this.add(main);
-        
-
     }
 
     public static void launch() {
